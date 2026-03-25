ec465d2a7af8d244d0bfc09d64232db925380293
siddhusingh21467-lab <siddhusingh21467@gmail.com>
2026-03-25 09:11:21 +0530

    UC7 COMPLETED

---
+++ UC7.java
@@ -0,0 +1,17 @@
+
+
+public class UC7{
+
+    public static void main(String[] args) {
+
+        String names;
+
+        if (args.length == 0) {
+            names = "World";
+        } else {
+            names = String.join(", ", args);
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
